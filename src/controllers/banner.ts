b45148f9cb77f215ddfc9e402d635c64cfff2fe0
Agash Thamotharampillai <agashthamo@Agashs-MacBook-Pro.local>
2019-02-01 20:43:45 +0100

    added flag to actually rotate the banner

--- src/controllers/banner.ts
+++ src/controllers/banner.ts
@@ -1,4 +1,3 @@
-import { Rarity } from "../models/pokemon/rarity";
 import PokeDex from "./pokedex";
 import { Pokemons } from "../models/pokemon/pokemon";
 import Messenger from "./messenger";
@@ -6,7 +5,7 @@ import Messenger from "./messenger";
 export default class Banner {
 
     constructor() {
-        cb.setTimeout(() => this.sendBanner(), cb.settings.banner_rotate * 1000)
+        cb.setTimeout(() => this.sendBanner(undefined, true), cb.settings.banner_rotate * 1000)
     }
 
     private startMessage =  `Pokemon - Gotta Catch 'Em All (with Tokens :P)!
@@ -16,7 +15,7 @@ export default class Banner {
                             '/release' to remove your Pokemon :(...
                             Prices:\n`;
 
-    public sendBanner(user?: string): void {
+    public sendBanner(user?: string, rotate?: boolean): void {
         const tempPrices = [cb.settings.catch_pokemon, cb.settings.uncommon_tip, cb.settings.rare_tip, cb.settings.legendary_tip, cb.settings.mystic_tip];
         let pricesMessage: string = "";
 
@@ -26,5 +25,9 @@ export default class Banner {
         }
 
         Messenger.sendInfoMessage(this.startMessage + pricesMessage + "Let the Battles Begin!", user);
+        
+        if(rotate !== undefined && rotate) {
+            cb.setTimeout(() => this.sendBanner(user, rotate), cb.settings.banner_rotate * 1000);
+        }
     }
 }
